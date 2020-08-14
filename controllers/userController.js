f21730188904c531d21a6acc57b5791c2230ae80
serie5 <nastybitsz@gmail.com>
2020-08-13 21:52:23 -0400

    corregir el error de postman

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,26 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const {validationResult} = require('express-validator');
 
 exports.newUser = async(req, res) => {
-    
-    //  verificar si el usuario ya estuvo registrado
+    // mostrar los mensajes de validacion
+    const errores = validationResult(req);
+    if (!errores.isEmpty) {
+        return res.status(400).json({errores: errores.array()});
+    }
     const {email, password} = req.body;
-
+    
     let user = await User.findOne({email});
     
-    if(user){
-        return res.status(400).json({msg:'El usuario ya esta registrado'});
+    if (user) {
+        return res.status(400).json({msg: 'El usuario ya esta registrado'});
     }
-    // creando un nuevo usuario
-    user = await new User(req.body);
-
+    // crear un nuevo usuario
+    user = new User(req.body);
+    
     // encriptar el password
     const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
+    user.password = await bcrypt.hash(password, salt);    
 
     try {
         await user.save();
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const userController= require('../controllers/userController');
 const router = express.Router();
+const {check} = require('express-validator');
 
 router.post('/',
+    [
+        check('name','El nombre es obligatorio').not().isEmpty()
+    ],
     userController.newUser
 );
 module.exports = router;
